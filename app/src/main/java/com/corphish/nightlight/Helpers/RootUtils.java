be5fa5c5743fb322ad48904583c0b4e8923bc09b
Avinaba Dalal <d97.avinaba@gmail.com>
2017-10-05 20:43:08 +0530

    RootUtils: Add docs

--- app/src/main/java/com/corphish/nightlight/Helpers/RootUtils.java
+++ app/src/main/java/com/corphish/nightlight/Helpers/RootUtils.java
@@ -8,10 +8,20 @@ import eu.chainfire.libsuperuser.Shell;
  */
 public class RootUtils {
 
+    /**
+     * Gets root access.
+     * @return - A boolean indicating whether root access is available or not.
+     */
     public static boolean getRootAccess() {
         return Shell.SU.available();
     }
 
+    /**
+     * Writes text to a file as root.
+     * It overwrites the contents of file, it does not append it.
+     * @param textToBeWritten - The text that is to be written.
+     * @param file - The file in which the text would be written.
+     */
     public static void writeToFile(String textToBeWritten, String file) {
         String command = "echo \"" + textToBeWritten + "\" > " + file;
         Shell.SU.run(command);

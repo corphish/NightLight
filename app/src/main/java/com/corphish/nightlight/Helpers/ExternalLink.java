779d6dc512d033a5abd45e0f5fb17993704708cc
Avinaba Dalal <d97.avinaba@gmail.com>
2017-10-14 18:43:04 +0530

    ExternalLink: Add helper class to open external link

---
+++ app/src/main/java/com/corphish/nightlight/Helpers/ExternalLink.java
@@ -0,0 +1,31 @@
+package com.corphish.nightlight.Helpers;
+
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+
+/**
+ * Created by Avinaba on 5/8/2017.
+ * To open external links
+ */
+
+public class ExternalLink {
+    private static void open(Context context, Intent intent) {
+        try {
+            context.startActivity(intent);
+        } catch (ActivityNotFoundException ignored) {}
+    }
+
+    public static void open(Context context, String url) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        open(context, intent);
+    }
+
+    public static void open(Context context, String url, int intentFlags) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        intent.addFlags(intentFlags);
+
+        open(context, intent);
+    }
+}

6e9494d64eff0dd812cd7bfa1a69a1684d250b8f
Avinaba Dalal <d97.avinaba@gmail.com>
2017-10-25 01:30:06 +0530

    MasterSwitchFragment: Cleanup

--- app/src/main/java/com/corphish/nightlight/UI/Fragments/MasterSwitchFragment.java
+++ app/src/main/java/com/corphish/nightlight/UI/Fragments/MasterSwitchFragment.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.widget.SwitchCompat;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;

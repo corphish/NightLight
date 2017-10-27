16adc26bb0ef44dad61f599cd69f6b0a0344a864
Avinaba Dalal <d97.avinaba@gmail.com>
2017-10-28 01:32:14 +0530

    LocationUtils: Add more functions
    - Add methods to get currentLocation.
    - Above function is added because often getLastKnownLocation reports either null or (0,0)
    - Also add helper function to check stale locations

--- app/src/main/java/com/corphish/nightlight/Helpers/LocationUtils.java
+++ app/src/main/java/com/corphish/nightlight/Helpers/LocationUtils.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v4.content.ContextCompat;
 
@@ -22,20 +23,27 @@ public class LocationUtils {
         Location bestLocation = null;
         try {
             for (String provider : providers) {
+                if (provider.equals(LocationManager.PASSIVE_PROVIDER)) continue;
                 Location l = mLocationManager.getLastKnownLocation(provider);
-                if (l == null) {
-                    continue;
-                }
-                if (bestLocation == null || l.getAccuracy() < bestLocation.getAccuracy()) {
-                    // Found best last known location: %s", l);
-                    bestLocation = l;
-                }
+                if (l == null) continue;
+                if (bestLocation == null || l.getAccuracy() < bestLocation.getAccuracy()) bestLocation = l;
             }
         } catch (SecurityException ignored) {}
 
 
 
         return bestLocation;
+    }
+
+    public static void requestCurrentLocation(Context context, LocationListener locationListener) {
+        LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        try {
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000 * 60, 1000, locationListener);
+        } catch (SecurityException ignored) {}
+    }
+
+    public static boolean isLocationStale(Location location) {
+        return location.getLatitude() == 0.0 && location.getLongitude() == 0.0;
     }
 
     public static boolean areLocationPermissionsAvailable(Context context) {

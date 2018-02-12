38ddff0d682d4578fe47e6eea8a217ac98562c4c
Avinaba Dalal <d97.avinaba@gmail.com>
2018-02-12 19:08:09 +0530

    NightLightSettingModeListener: Initial commit
    Signed-off-by: Avinaba Dalal <d97.avinaba@gmail.com>

---
+++ app/src/main/java/com/corphish/nightlight/interfaces/NightLightSettingModeListener.java
@@ -0,0 +1,10 @@
+package com.corphish.nightlight.interfaces;
+
+/**
+ * Created by avinabadalal on 12/02/18.
+ * Night light setting mode listener
+ */
+
+public interface NightLightSettingModeListener {
+    void onModeChanged(int newMode);
+}

c46c36df134662c033cea6dfa5ec29856c511458
Avinaba Dalal <d97.avinaba@gmail.com>
2018-02-19 19:37:32 +0530

    AdvancedAutomation: Remove logging
    Signed-off-by: Avinaba Dalal <d97.avinaba@gmail.com>

--- app/src/main/java/com/corphish/nightlight/design/fragments/AdvancedAutomationFragment.java
+++ app/src/main/java/com/corphish/nightlight/design/fragments/AdvancedAutomationFragment.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.SwitchCompat;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;

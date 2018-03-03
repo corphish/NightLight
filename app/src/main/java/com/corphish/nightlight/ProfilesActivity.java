5ff4f6a2b630cf6f4d3bae1a8d17cb5df2b6e378
Avinaba Dalal <d97.avinaba@gmail.com>
2018-03-03 22:33:03 +0530

    ProfilesActivity: Fix toolbar
    Signed-off-by: Avinaba Dalal <d97.avinaba@gmail.com>

--- app/src/main/java/com/corphish/nightlight/ProfilesActivity.java
+++ app/src/main/java/com/corphish/nightlight/ProfilesActivity.java
@@ -1,14 +1,21 @@
 package com.corphish.nightlight;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
-public class ProfilesActivity extends Activity {
+public class ProfilesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profiles);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }

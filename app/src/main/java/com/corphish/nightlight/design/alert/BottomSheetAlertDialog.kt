ee80308431eacf22decdff44e4382ade0084ed02
Avinaba Dalal <d97.avinaba@gmail.com>
2018-06-06 19:24:15 +0530

    BottomSheetAlertDialog: Improve null safety
    Signed-off-by: Avinaba Dalal <d97.avinaba@gmail.com>

--- app/src/main/java/com/corphish/nightlight/design/alert/BottomSheetAlertDialog.kt
+++ app/src/main/java/com/corphish/nightlight/design/alert/BottomSheetAlertDialog.kt
@@ -14,100 +14,92 @@ import com.corphish.nightlight.R
  * Bottom sheet alert dialog
  */
 
-class BottomSheetAlertDialog(private val context: Context) {
-    private var contentView: View? = null
+class BottomSheetAlertDialog(val context: Context) {
+    private var contentView = View.inflate(context, R.layout.bottom_sheet_msg, null)
+    private var title: TextView
+    private var content: TextView
+    private var negativeButton: TextView
+    private var positiveButton: AppCompatButton
+    private var neutralButton: AppCompatButton
 
-    private var title: TextView? = null
-    private var content: TextView? = null
-    private var negativeButton: TextView? = null
-    private var positiveButton: AppCompatButton? = null
-    private var neutralButton: AppCompatButton? = null
-
-    private var bottomSheetDialog: com.google.android.material.bottomsheet.BottomSheetDialog? = null
+    private var bottomSheetDialog: BottomSheetDialog
 
     init {
+        title = contentView.findViewById(R.id.title)
+        content = contentView.findViewById(R.id.content)
+        negativeButton = contentView.findViewById(R.id.negativeButton)
+        positiveButton = contentView.findViewById(R.id.positiveButton)
+        neutralButton = contentView.findViewById(R.id.neutralButton)
 
-        init()
-    }
-
-    private fun init() {
-        contentView = View.inflate(context, R.layout.bottom_sheet_msg, null)
-
-        title = contentView!!.findViewById(R.id.title)
-        content = contentView!!.findViewById(R.id.content)
-        negativeButton = contentView!!.findViewById(R.id.negativeButton)
-        positiveButton = contentView!!.findViewById(R.id.positiveButton)
-        neutralButton = contentView!!.findViewById(R.id.neutralButton)
-
-        bottomSheetDialog = com.google.android.material.bottomsheet.BottomSheetDialog(context, R.style.BottomSheetDialogDark)
+        bottomSheetDialog = BottomSheetDialog(context, R.style.BottomSheetDialogDark)
     }
 
     fun setTitle(@StringRes iTitle: Int): BottomSheetAlertDialog {
-        title!!.setText(iTitle)
+        title.setText(iTitle)
 
         return this
     }
 
     fun setTitle(sTitle: String): BottomSheetAlertDialog {
-        title!!.text = sTitle
+        title.text = sTitle
 
         return this
     }
 
     fun setMessage(@StringRes iTitle: Int): BottomSheetAlertDialog {
-        content!!.setText(iTitle)
+        content.setText(iTitle)
 
         return this
     }
 
     fun setMessage(sTitle: String): BottomSheetAlertDialog {
-        content!!.text = sTitle
+        content.text = sTitle
 
         return this
     }
 
     fun setPositiveButton(@StringRes iTitle: Int, onClickListener: View.OnClickListener): BottomSheetAlertDialog {
-        positiveButton!!.visibility = View.VISIBLE
-        positiveButton!!.setText(iTitle)
-        positiveButton!!.setOnClickListener { view ->
+        positiveButton.visibility = View.VISIBLE
+        positiveButton.setText(iTitle)
+        positiveButton.setOnClickListener { view ->
             onClickListener.onClick(view)
-            bottomSheetDialog!!.dismiss()
+            bottomSheetDialog.dismiss()
         }
 
         return this
     }
 
     fun setNegativeButton(@StringRes iTitle: Int, onClickListener: View.OnClickListener): BottomSheetAlertDialog {
-        negativeButton!!.visibility = View.VISIBLE
-        negativeButton!!.setText(iTitle)
-        negativeButton!!.setOnClickListener { view ->
+        negativeButton.visibility = View.VISIBLE
+        negativeButton.setText(iTitle)
+        negativeButton.setOnClickListener { view ->
             onClickListener.onClick(view)
-            bottomSheetDialog!!.dismiss()
+            bottomSheetDialog.dismiss()
         }
 
         return this
     }
 
     fun setNeutralButton(@StringRes iTitle: Int, onClickListener: View.OnClickListener): BottomSheetAlertDialog {
-        neutralButton!!.visibility = View.VISIBLE
-        neutralButton!!.setText(iTitle)
-        neutralButton!!.setOnClickListener { view ->
+        neutralButton.visibility = View.VISIBLE
+        neutralButton.setText(iTitle)
+        neutralButton.setOnClickListener { view ->
             onClickListener.onClick(view)
-            bottomSheetDialog!!.dismiss()
+            bottomSheetDialog.dismiss()
         }
 
         return this
     }
 
     fun setCancelable(b: Boolean): BottomSheetAlertDialog {
-        bottomSheetDialog!!.setCancelable(b)
+        bottomSheetDialog.setCancelable(b)
 
         return this
     }
 
     fun show(): Boolean {
-        bottomSheetDialog!!.setContentView(contentView)
-        bottomSheetDialog!!.show()
+        bottomSheetDialog.setContentView(contentView)
+        bottomSheetDialog.show()
 
         return true
     }

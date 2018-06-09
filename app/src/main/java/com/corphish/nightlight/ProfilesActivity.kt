da91b74a662f1f6f5a8f0caf67bf57fcb47f6ab6
Avinaba Dalal <d97.avinaba@gmail.com>
2018-06-10 00:39:47 +0530

    ProfileCreator: Move profile creator form to its own unit
    Signed-off-by: Avinaba Dalal <d97.avinaba@gmail.com>

--- app/src/main/java/com/corphish/nightlight/ProfilesActivity.kt
+++ app/src/main/java/com/corphish/nightlight/ProfilesActivity.kt
@@ -3,56 +3,28 @@ package com.corphish.nightlight
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.bottomsheet.BottomSheetDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.AppCompatButton
-import androidx.appcompat.widget.AppCompatEditText
-import androidx.appcompat.widget.AppCompatSeekBar
-import androidx.appcompat.widget.AppCompatSpinner
-import androidx.recyclerview.widget.DefaultItemAnimator
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import androidx.appcompat.widget.SwitchCompat
-import androidx.appcompat.widget.Toolbar
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
 import android.widget.TextView
-import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.RecyclerView
 
 import com.corphish.nightlight.data.Constants
 import com.corphish.nightlight.design.alert.BottomSheetAlertDialog
+import com.corphish.nightlight.design.views.ProfileCreator
 import com.corphish.nightlight.engine.ProfilesManager
 import com.corphish.nightlight.helpers.PreferenceHelper
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 import java.util.Arrays
 
 class ProfilesActivity : AppCompatActivity(), ProfilesManager.DataChangeListener {
 
-    private var creatorView: View? = null
-    private var editText: AppCompatEditText? = null
-    private var editTextError: TextView? = null
-    private var settingTitle1: TextView? = null
-    private var settingTitle2: TextView? = null
-    private var cancel: TextView? = null
-    private var profileAction: TextView? = null
-    private var profileActionDesc: TextView? = null
-    private var modes: AppCompatSpinner? = null
-    private var settingParam1: AppCompatSeekBar? = null
-    private var settingParam2: AppCompatSeekBar? = null
-    private var ok: AppCompatButton? = null
-    private var nlSwitch: SwitchCompat? = null
-    private var bottomSheetDialog: com.google.android.material.bottomsheet.BottomSheetDialog? = null
-    private var optionsDialog: com.google.android.material.bottomsheet.BottomSheetDialog? = null
+    private var optionsDialog: BottomSheetDialog? = null
     private var optionsView: View? = null
 
-    private var currentModeSelection = Constants.NL_SETTING_MODE_FILTER
     private var curProfile: ProfilesManager.Profile? = null
-
-    private val MODE_CREATE = 0
-    private val MODE_EDIT = 1
-    private var curMode = MODE_CREATE
 
     private var profilesManager: ProfilesManager? = null
 
@@ -71,13 +43,10 @@ class ProfilesActivity : AppCompatActivity(), ProfilesManager.DataChangeListener
         context = this
 
         findViewById<View>(R.id.fab).setOnClickListener {
-            curProfile = null
-            curMode = MODE_CREATE
-            bottomSheetDialog = com.google.android.material.bottomsheet.BottomSheetDialog(this@ProfilesActivity, R.style.BottomSheetDialogDark)
-            initProfileCreatorViews()
-            bottomSheetDialog!!.setContentView(creatorView)
-            bottomSheetDialog!!.setCancelable(false)
-            bottomSheetDialog!!.show()
+            ProfileCreator(this@ProfilesActivity, ProfileCreator.MODE_CREATE, null, {
+                profiles = profilesManager?.profilesList
+                profilesAdapter?.notifyDataSetChanged()
+            }).show()
         }
 
         findViewById<TextView>(R.id.banner_title).text = getString(R.string.banner_app_name, BuildConfig.VERSION_NAME)
@@ -117,7 +86,7 @@ class ProfilesActivity : AppCompatActivity(), ProfilesManager.DataChangeListener
             emptyView!!.visibility = View.GONE
     }
 
-    private inner class ProfilesAdapter : androidx.recyclerview.widget.RecyclerView.Adapter<ProfilesActivity.ProfilesAdapter.CustomViewHolder>() {
+    private inner class ProfilesAdapter : RecyclerView.Adapter<ProfilesActivity.ProfilesAdapter.CustomViewHolder>() {
         private var profiles: List<ProfilesManager.Profile>? = null
 
         internal fun setProfiles(profiles: MutableList<ProfilesManager.Profile>?) {
@@ -125,14 +94,10 @@ class ProfilesActivity : AppCompatActivity(), ProfilesManager.DataChangeListener
         }
 
         inner class CustomViewHolder internal constructor(v: View) : androidx.recyclerview.widget.RecyclerView.ViewHolder(v), View.OnClickListener {
-            internal val name: TextView
-            internal val desc: TextView
+            internal val name: TextView = v.findViewById(R.id.profile_name)
+            internal val desc: TextView = v.findViewById(R.id.profile_desc)
 
             init {
-
-                name = v.findViewById(R.id.profile_name)
-                desc = v.findViewById(R.id.profile_desc)
-
                 v.setOnClickListener(this)
             }
 
@@ -142,7 +107,7 @@ class ProfilesActivity : AppCompatActivity(), ProfilesManager.DataChangeListener
                 } else {
                     curProfile = profiles!![adapterPosition]
                     optionsDialog = com.google.android.material.bottomsheet.BottomSheetDialog(this@ProfilesActivity, R.style.BottomSheetDialogDark)
-                    getOptionsView(curProfile!!.name)
+                    getOptionsView(curProfile!!)
                     optionsDialog!!.setContentView(optionsView)
                     optionsDialog!!.show()
                 }
@@ -176,107 +141,12 @@ class ProfilesActivity : AppCompatActivity(), ProfilesManager.DataChangeListener
         }
     }
 
-    private fun initProfileCreatorViews() {
-        creatorView = View.inflate(this, R.layout.bottom_sheet_create_profile, null)
 
-        profileAction = creatorView!!.findViewById(R.id.profile_action_title)
-        profileActionDesc = creatorView!!.findViewById(R.id.profile_action_desc)
-        editText = creatorView!!.findViewById(R.id.profile_name_set)
-        editTextError = creatorView!!.findViewById(R.id.profile_name_error)
-        nlSwitch = creatorView!!.findViewById(R.id.profile_night_light_switch)
-        settingTitle1 = creatorView!!.findViewById(R.id.profile_night_light_setting_title1)
-        settingTitle2 = creatorView!!.findViewById(R.id.profile_night_light_setting_title2)
-        cancel = creatorView!!.findViewById(R.id.button_cancel)
-        modes = creatorView!!.findViewById(R.id.profile_night_light_setting_mode)
-        settingParam1 = creatorView!!.findViewById(R.id.profile_night_light_setting_param1)
-        settingParam2 = creatorView!!.findViewById(R.id.profile_night_light_setting_param2)
-        ok = creatorView!!.findViewById(R.id.button_ok)
 
-        modes!!.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
-                currentModeSelection = if (curProfile == null) position else curProfile!!.settingMode
-                updateProfileCreatorParams(currentModeSelection, curProfile)
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>) {
-
-            }
-        }
-
-        cancel!!.setOnClickListener { bottomSheetDialog!!.dismiss() }
-
-        ok!!.setOnClickListener {
-            val retVal = if (curMode == MODE_CREATE) profilesManager!!.createProfile(nlSwitch!!.isChecked,
-                        editText!!.editableText.toString(),
-                        modes!!.selectedItemPosition,
-                        if (modes!!.selectedItemId == Constants.NL_SETTING_MODE_TEMP.toLong()) intArrayOf(settingParam1!!.progress + 3000) else intArrayOf(settingParam1!!.progress, settingParam2!!.progress))
-            else profilesManager!!.updateProfile(curProfile!!.name,
-                        nlSwitch!!.isChecked,
-                        editText!!.editableText.toString(),
-                        modes!!.selectedItemPosition,
-                        if (modes!!.selectedItemId == Constants.NL_SETTING_MODE_TEMP.toLong()) intArrayOf(settingParam1!!.progress + 3000) else intArrayOf(settingParam1!!.progress, settingParam2!!.progress))
-            if (retVal) {
-                profiles = profilesManager!!.profilesList
-                profilesAdapter!!.notifyDataSetChanged()
-                bottomSheetDialog!!.dismiss()
-            } else
-                editTextError!!.visibility = View.VISIBLE
-        }
-
-        if (curProfile != null) {
-            nlSwitch!!.isChecked = curProfile!!.isSettingEnabled
-            editText!!.setText(curProfile!!.name)
-            modes!!.setSelection(curProfile!!.settingMode)
-        }
-
-        profileAction!!.setText(
-                if (curMode == MODE_CREATE) R.string.profile_create_title else R.string.profile_edit_title
-        )
-
-        profileActionDesc!!.setText(
-                if (curMode == MODE_CREATE) R.string.profile_create_desc else R.string.profile_edit_desc
-        )
-
-        editTextError!!.visibility = View.GONE
-    }
-
-    private fun updateProfileCreatorParams(mode: Int, profile: ProfilesManager.Profile?) {
-        if (mode == Constants.NL_SETTING_MODE_FILTER) {
-            settingParam1!!.isEnabled = true
-            settingParam1!!.max = 128
-            settingParam2!!.isEnabled = true
-            settingParam2!!.max = 48
-            settingTitle1!!.isEnabled = true
-            settingTitle2!!.isEnabled = true
-            settingTitle1!!.setText(R.string.blue_light)
-            settingTitle2!!.setText(R.string.green_light)
-            if (profile != null) {
-                settingParam1!!.progress = profile.settings[0]
-                settingParam2!!.progress = profile.settings[1]
-            } else {
-                settingParam1!!.progress = PreferenceHelper.getInt(this, Constants.PREF_BLUE_INTENSITY, Constants.DEFAULT_BLUE_INTENSITY)
-                settingParam2!!.progress = PreferenceHelper.getInt(this, Constants.PREF_GREEN_INTENSITY, Constants.DEFAULT_GREEN_INTENSITY)
-            }
-        } else {
-            settingParam1!!.isEnabled = true
-            settingParam1!!.max = 1500
-            settingParam2!!.isEnabled = false
-            settingTitle1!!.isEnabled = true
-            settingTitle2!!.isEnabled = false
-            settingTitle1!!.setText(R.string.color_temperature_title)
-            settingTitle2!!.setText(R.string.profile_nl_setting_unavailable)
-            if (profile != null) {
-                settingParam1!!.progress = profile.settings[0] - 3000
-            } else {
-                settingParam1!!.progress = PreferenceHelper.getInt(this, Constants.PREF_COLOR_TEMP, Constants.DEFAULT_COLOR_TEMP)
-            }
-        }
-    }
-
-    private fun getOptionsView(profileName: String?) {
+    private fun getOptionsView(profile: ProfilesManager.Profile?) {
         optionsView = View.inflate(this, R.layout.bottom_sheet_profile_options, null)
 
-        (optionsView!!.findViewById<View>(R.id.selected_profile_name) as TextView).text = profileName
+        (optionsView!!.findViewById<View>(R.id.selected_profile_name) as TextView).text = profile?.name
 
         optionsView!!.findViewById<View>(R.id.apply).setOnClickListener {
             if (curProfile != null) {
@@ -290,12 +160,7 @@ class ProfilesActivity : AppCompatActivity(), ProfilesManager.DataChangeListener
         }
 
         optionsView!!.findViewById<View>(R.id.edit).setOnClickListener {
-            curMode = MODE_EDIT
-            bottomSheetDialog = com.google.android.material.bottomsheet.BottomSheetDialog(this@ProfilesActivity, R.style.BottomSheetDialogDark)
-            initProfileCreatorViews()
-            bottomSheetDialog!!.setContentView(creatorView)
-            bottomSheetDialog!!.setCancelable(false)
-            bottomSheetDialog!!.show()
+            ProfileCreator(this@ProfilesActivity, ProfileCreator.MODE_EDIT, profile, {}).show()
             optionsDialog!!.dismiss()
         }
 
---
+++ app/src/main/java/com/corphish/nightlight/design/views/ProfileCreator.kt
@@ -0,0 +1,176 @@
+package com.corphish.nightlight.design.views
+
+import android.content.Context
+import android.view.View
+import android.widget.AdapterView
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatSeekBar
+import androidx.appcompat.widget.AppCompatSpinner
+import androidx.appcompat.widget.SwitchCompat
+import com.corphish.nightlight.R
+import com.corphish.nightlight.data.Constants
+import com.corphish.nightlight.engine.ProfilesManager
+import com.corphish.nightlight.helpers.PreferenceHelper
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+
+class ProfileCreator(val context: Context,
+                     private val operationMode: Int,
+                     val profile: ProfilesManager.Profile? = null,
+                     private val onFinishListener: (status: Int) -> Unit) {
+
+    private val creatorView = View.inflate(context, R.layout.bottom_sheet_create_profile, null)
+
+    private val profileAction = creatorView.findViewById<TextView>(R.id.profile_action_title)
+    private val profileActionDesc = creatorView.findViewById<TextView>(R.id.profile_action_desc)
+
+    private val editText = creatorView.findViewById<TextInputEditText>(R.id.profile_name_set)
+    private val editTextLayout = creatorView.findViewById<TextInputLayout>(R.id.profile_name_set_layout)
+
+    private val nlSwitch = creatorView.findViewById<SwitchCompat>(R.id.profile_night_light_switch)
+
+    private val settingTitle1 = creatorView.findViewById<TextView>(R.id.profile_night_light_setting_title1)
+    private val settingTitle2 = creatorView.findViewById<TextView>(R.id.profile_night_light_setting_title2)
+
+    private val cancel = creatorView.findViewById<TextView>(R.id.button_cancel)
+    private val ok = creatorView.findViewById<AppCompatButton>(R.id.button_ok)
+
+    private val modes = creatorView.findViewById<AppCompatSpinner>(R.id.profile_night_light_setting_mode)
+
+    private val settingParam1 = creatorView.findViewById<AppCompatSeekBar>(R.id.profile_night_light_setting_param1)
+    private val settingParam2 = creatorView.findViewById<AppCompatSeekBar>(R.id.profile_night_light_setting_param2)
+
+    private val bottomSheetDialog = BottomSheetDialog(context, R.style.BottomSheetDialogDark)
+
+    private val profilesManager = ProfilesManager(context)
+
+    private fun initViewEventListeners() {
+        modes.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                val currentModeSelection = profile?.settingMode ?: position
+                updateProfileCreatorParams(currentModeSelection)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {}
+        }
+
+        cancel.setOnClickListener {
+            onFinishListener.invoke(STATUS_CANCELED)
+            bottomSheetDialog.dismiss()
+        }
+
+        ok.setOnClickListener {
+            val retVal = if (operationMode == MODE_CREATE)
+                createProfileWithCurrentSelections()
+            else
+                updateProfileWithCurrentSelections()
+
+            if (retVal)
+                onFinishListener(STATUS_SUCCESS)
+            else
+                editTextLayout.error = context.getString(R.string.profile_create_name_error)
+        }
+    }
+
+    private fun initViews() {
+        if (profile != null) {
+            nlSwitch.isChecked = profile.isSettingEnabled
+            editText.setText(profile.name)
+            modes.setSelection(profile.settingMode)
+        }
+
+        profileAction.setText(
+                if (operationMode == MODE_CREATE) R.string.profile_create_title else R.string.profile_edit_title
+        )
+
+        profileActionDesc.setText(
+                if (operationMode == MODE_CREATE) R.string.profile_create_desc else R.string.profile_edit_desc
+        )
+
+        editTextLayout.error = null
+    }
+
+    private fun createProfileWithCurrentSelections(): Boolean {
+        return profilesManager.createProfile(
+                nlSwitch.isChecked,
+                editText.editableText.toString(),
+                modes.selectedItemPosition,
+                if (modes.selectedItemId == Constants.NL_SETTING_MODE_TEMP.toLong())
+                    intArrayOf(settingParam1.progress + 3000)
+                else
+                    intArrayOf(settingParam1.progress, settingParam2.progress)
+        )
+    }
+
+    private fun updateProfileWithCurrentSelections(): Boolean {
+        return  profilesManager.updateProfile(
+                profile!!.name,
+                nlSwitch.isChecked,
+                editText.editableText.toString(),
+                modes.selectedItemPosition,
+                if (modes.selectedItemId == Constants.NL_SETTING_MODE_TEMP.toLong())
+                    intArrayOf(settingParam1.progress + 3000)
+                else
+                    intArrayOf(settingParam1.progress, settingParam2.progress))
+    }
+
+    private fun updateProfileCreatorParams(mode: Int) {
+        if (mode == Constants.NL_SETTING_MODE_FILTER) {
+            settingParam1.isEnabled = true
+            settingParam1.max = 128
+
+            settingParam2.isEnabled = true
+            settingParam2.max = 48
+
+            settingTitle1.isEnabled = true
+            settingTitle2.isEnabled = true
+
+            settingTitle1.setText(R.string.blue_light)
+            settingTitle2.setText(R.string.green_light)
+
+            if (profile != null) {
+                settingParam1.progress = profile.settings[0]
+                settingParam2.progress = profile.settings[1]
+            } else {
+                settingParam1.progress = PreferenceHelper.getInt(context, Constants.PREF_BLUE_INTENSITY, Constants.DEFAULT_BLUE_INTENSITY)
+                settingParam2.progress = PreferenceHelper.getInt(context, Constants.PREF_GREEN_INTENSITY, Constants.DEFAULT_GREEN_INTENSITY)
+            }
+        } else {
+            settingParam1.isEnabled = true
+            settingParam1.max = 1500
+
+            settingParam2.isEnabled = false
+
+            settingTitle1.isEnabled = true
+            settingTitle2.isEnabled = false
+
+            settingTitle1.setText(R.string.color_temperature_title)
+            settingTitle2.setText(R.string.profile_nl_setting_unavailable)
+
+            if (profile != null) {
+                settingParam1.progress = profile.settings[0] - 3000
+            } else {
+                settingParam1.progress = PreferenceHelper.getInt(context, Constants.PREF_COLOR_TEMP, Constants.DEFAULT_COLOR_TEMP)
+            }
+        }
+    }
+
+    fun show() {
+        initViews()
+        initViewEventListeners()
+
+        bottomSheetDialog.setContentView(creatorView)
+        bottomSheetDialog.setCancelable(false)
+        bottomSheetDialog.show()
+    }
+
+    companion object {
+        const val MODE_CREATE = 0
+        const val MODE_EDIT = 1
+
+        const val STATUS_SUCCESS = 0
+        const val STATUS_CANCELED = 1
+    }
+}

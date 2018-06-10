09b79f61359dab263ae6c81c939ec4002785d8fa
Avinaba Dalal <d97.avinaba@gmail.com>
2018-06-10 23:44:40 +0530

    font: Switch to PT Sans
    Signed-off-by: Avinaba Dalal <d97.avinaba@gmail.com>

--- app/src/main/java/com/corphish/nightlight/design/utils/FontUtils.kt
+++ app/src/main/java/com/corphish/nightlight/design/utils/FontUtils.kt
@@ -1,6 +1,7 @@
 package com.corphish.nightlight.design.utils
 
 import android.content.Context
+import android.content.res.Resources
 import android.view.View
 import androidx.appcompat.widget.AppCompatCheckBox
 import androidx.appcompat.widget.SwitchCompat
@@ -9,11 +10,13 @@ import com.corphish.nightlight.R
 
 class FontUtils {
     fun setCustomFont(context: Context, vararg views: View?) {
-        for (view in views) {
-            when (view) {
-                is SwitchCompat? -> view?.typeface = ResourcesCompat.getFont(context, R.font.acme)
-                is AppCompatCheckBox? -> view?.typeface = ResourcesCompat.getFont(context, R.font.acme)
+        try {
+            for (view in views) {
+                when (view) {
+                    is SwitchCompat? -> view?.typeface = ResourcesCompat.getFont(context, R.font.cust_font)
+                    is AppCompatCheckBox? -> view?.typeface = ResourcesCompat.getFont(context, R.font.cust_font)
+                }
             }
-        }
+        } catch (e: Resources.NotFoundException) {}
     }
 }

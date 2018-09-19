cf99627f8d3520ffe4ab60532a5beeb9f53671f5
Avinaba Dalal <d97.avinaba@gmail.com>
2018-09-19 20:46:30 +0530

    KCALAbstraction: Initial commit
    Signed-off-by: Avinaba Dalal <d97.avinaba@gmail.com>

---
+++ app/src/main/java/com/corphish/nightlight/engine/kcal/KCALAbstraction.kt
@@ -0,0 +1,27 @@
+package com.corphish.nightlight.engine.kcal
+
+/*
+ * KCAL Abstraction
+ * Objective of this abstraction is to provide a generic way to work with various KCAL implementations
+ */
+interface KCALAbstraction {
+    /**
+     * A function to determine whether the implementation is supported by device
+     */
+    fun isSupported() : Boolean
+
+    /**
+     * A function to turn on KCAL
+     */
+    fun turnOn()
+
+    /**
+     * A function to adjust KCAL colors
+     */
+    fun setColors(red: Int, green: Int, blue: Int)
+
+    /**
+     * A function get current KCAL color readings
+     */
+    fun getColorReadings() : IntArray
+}

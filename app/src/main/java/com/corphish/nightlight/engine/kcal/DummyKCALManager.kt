91215c67662315b283b9fd1cd4f124ebb90e45ec
Avinaba Dalal <d97.avinaba@gmail.com>
2018-09-19 21:20:50 +0530

    kcal: Add dummy kcal manager
    Signed-off-by: Avinaba Dalal <d97.avinaba@gmail.com>

---
+++ app/src/main/java/com/corphish/nightlight/engine/kcal/DummyKCALManager.kt
@@ -0,0 +1,32 @@
+package com.corphish.nightlight.engine.kcal
+
+/*
+ * Dummy KCAL manager to handle devices without KCAL support
+ */
+class DummyKCALManager : KCALAbstraction {
+    /**
+     * A function to determine whether the implementation is supported by device
+     */
+    override fun isSupported(): Boolean = false
+
+    /**
+     * A function to turn on KCAL
+     */
+    override fun turnOn() {
+        // Not supported
+    }
+
+    /**
+     * A function to adjust KCAL colors
+     */
+    override fun setColors(red: Int, green: Int, blue: Int) {
+        // Not supported
+    }
+
+    /**
+     * A function get current KCAL color readings
+     */
+    override fun getColorReadings(): IntArray {
+        return intArrayOf(0, 0, 0)
+    }
+}

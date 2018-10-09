ccc4e5d6f6e79366762d1b76caba9f556ab47ae2
Avinaba Dalal <d97.avinaba@gmail.com>
2018-10-09 13:14:24 +0530

    Constants: Add min and max intensity dimension
    Signed-off-by: Avinaba Dalal <d97.avinaba@gmail.com>

--- app/src/main/java/com/corphish/nightlight/data/Constants.kt
+++ app/src/main/java/com/corphish/nightlight/data/Constants.kt
@@ -11,9 +11,12 @@ object Constants {
      */
     const val PREF_MASTER_SWITCH = "switch"
     const val PREF_FORCE_SWITCH = "force_switch"
-    const val PREF_BLUE_COLOR = "blue_color"
-    const val PREF_GREEN_COLOR = "green_color"
-    const val PREF_RED_COLOR = "red_color"
+    const val PREF_MAX_BLUE_COLOR = "max_blue_color"
+    const val PREF_MAX_GREEN_COLOR = "max_green_color"
+    const val PREF_MAX_RED_COLOR = "max_red_color"
+    const val PREF_MIN_BLUE_COLOR = "min_blue_color"
+    const val PREF_MIN_GREEN_COLOR = "min_green_color"
+    const val PREF_MIN_RED_COLOR = "min_red_color"
     const val PREF_AUTO_SWITCH = "auto_switch"
     const val PREF_SUN_SWITCH = "sun_switch"
     const val PREF_START_TIME = "start_time"
@@ -26,7 +29,8 @@ object Constants {
     const val KCAL_PRESERVE_SWITCH = "kcal_preserve_switch"
     const val KCAL_PRESERVE_VAL = "kcal_preserve_const val"
     const val PREF_SETTING_MODE = "nl_setting_mode"
-    const val PREF_COLOR_TEMP = "color_temp_const val"
+    const val PREF_MAX_COLOR_TEMP = "max_color_temp"
+    const val PREF_MIN_COLOR_TEMP = "min_color_temp"
     const val PREF_BOOT_MODE = "boot_mode"
     const val PREF_LAST_BOOT_RES = "last_boot_result"
     const val PREF_BOOT_DELAY = "boot_delay"
@@ -39,16 +43,21 @@ object Constants {
 
     /**
      * Fixed const values (default, max const values)
+     * For max color intensities, values may seem minimum and vice versa
      */
     const val DEFAULT_START_TIME = "00:00"
     const val DEFAULT_END_TIME = "06:00"
-    const val DEFAULT_BLUE_COLOR = 256 // TODO
-    const val DEFAULT_GREEN_COLOR = 256 // TODO
-    const val DEFAULT_RED_COLOR = 256 // TODO
+    const val DEFAULT_MAX_BLUE_COLOR = 256 // TODO
+    const val DEFAULT_MAX_GREEN_COLOR = 256 // TODO
+    const val DEFAULT_MAX_RED_COLOR = 256 // TODO
+    const val DEFAULT_MIN_BLUE_COLOR = 256 // TODO
+    const val DEFAULT_MIN_GREEN_COLOR = 256 // TODO
+    const val DEFAULT_MIN_RED_COLOR = 256 // TODO
     const val DEFAULT_LONGITUDE = "0.00"
     const val DEFAULT_LATITUDE = "0.00"
     const val DEFAULT_KCAL_VALUES = "256 256 256"
-    const val DEFAULT_COLOR_TEMP = 4000
+    const val DEFAULT_MAX_COLOR_TEMP = 4000
+    const val DEFAULT_MIN_COLOR_TEMP = 4500
     const val DEFAULT_BOOT_DELAY = 20
 
     /**
@@ -67,6 +76,5 @@ object Constants {
      * Tasker error in-app communication msg
      */
     const val TASKER_ERROR_STATUS = "tasker_error"
-
     const val TASKER_SETTING = "tasker_setting"
 }

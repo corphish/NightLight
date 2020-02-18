e38541e0145953ac97cf06f85474b7a1b5e4e726
corphish <d97.avinaba@gmail.com>
2020-02-18 22:38:29 +0530

    layout: master switch: Layout and text updates
    Signed-off-by: corphish <d97.avinaba@gmail.com>

--- app/src/main/java/com/corphish/nightlight/activities/MasterSwitchActivity.kt
+++ app/src/main/java/com/corphish/nightlight/activities/MasterSwitchActivity.kt
@@ -3,11 +3,13 @@ package com.corphish.nightlight.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.corphish.nightlight.R
+import com.corphish.nightlight.design.ThemeUtils
 
 class MasterSwitchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(ThemeUtils.getAppTheme(this))
         setContentView(R.layout.activity_master_switch)
     }
 }

f345067f912fdce751d5f4b76bd5d6aea6f55396
corphish <d97.avinaba@gmail.com>
2020-02-18 22:22:45 +0530

    layout: master_switch: Initial design

---
+++ app/src/main/java/com/corphish/nightlight/activities/MasterSwitchActivity.kt
@@ -0,0 +1,13 @@
+package com.corphish.nightlight.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.corphish.nightlight.R
+
+class MasterSwitchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_master_switch)
+    }
+}

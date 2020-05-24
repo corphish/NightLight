90b6ac7b5090571e95df51041d64a24a578fcb27
corphish <d97.avinaba@gmail.com>
2020-05-24 22:13:22 +0530

    SettingFragment: Show bed time mode only if grayscale is supported
    Signed-off-by: corphish <d97.avinaba@gmail.com>

--- app/src/main/java/com/corphish/nightlight/design/fragments/SettingFragment.kt
+++ app/src/main/java/com/corphish/nightlight/design/fragments/SettingFragment.kt
@@ -14,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.corphish.nightlight.R
 import com.corphish.nightlight.activities.*
 import com.corphish.nightlight.data.Constants
+import com.corphish.nightlight.engine.KCALManager
 import com.corphish.nightlight.helpers.ExternalLink
 import com.corphish.nightlight.helpers.PreferenceHelper
 import com.corphish.nightlight.services.NightLightAppService
@@ -63,14 +64,14 @@ class SettingFragment: DialogFragment() {
      */
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val settingsAdapter = SettingsAdapter()
-        settingsOptions = listOf(
+        settingsOptions = listOfNotNull(
                 SettingOption(R.string.section_main, R.drawable.ic_power, activityClass = MasterSwitchActivity::class.java),
                 SettingOption(R.string.section_color, R.drawable.ic_color, activityClass = ColorActivity::class.java),
-                SettingOption(R.string.bed_time_title, R.drawable.ic_bed_time_icon, activityClass = BedTimeActivity::class.java),
+                if (KCALManager.isGrayScaleSupported) SettingOption(R.string.bed_time_title, R.drawable.ic_bed_time_icon, activityClass = BedTimeActivity::class.java) else null,
                 SettingOption(R.string.section_auto, R.drawable.ic_alarm, activityClass = AutomationActivity::class.java),
                 SettingOption(R.string.section_kcal_backup, R.drawable.ic_settings_backup_restore, KCALBackupSettingsFragment()),
                 SettingOption(R.string.section_sob, R.drawable.ic_timer, SetOnBootDelayFragment()),
-                SettingOption(R.string.profile_title, R.drawable.ic_profiles, activityClass =  ProfilesActivity::class.java),
+                SettingOption(R.string.profile_title, R.drawable.ic_profiles, activityClass = ProfilesActivity::class.java),
                 SettingOption(R.string.kcal_driver_information_short, R.drawable.ic_driver, KCALDriverInfoFragment()),
                 SettingOption(R.string.options, R.drawable.ic_settings, OptionsFragment()),
                 SettingOption(R.string.show_support, R.drawable.ic_thumb_up, AppreciationFragment()),

85b98755d922b8ede34fa451af162a3ee8fad03e
corphish <d97.avinaba@gmail.com>
2020-05-25 13:13:52 +0530

    ProFragment: Cleanup imports
    Signed-off-by: corphish <d97.avinaba@gmail.com>

--- app/src/main/java/com/corphish/nightlight/design/fragments/ProFragment.kt
+++ app/src/main/java/com/corphish/nightlight/design/fragments/ProFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.corphish.nightlight.R
-import com.corphish.nightlight.design.alert.BottomSheetAlertDialog
 import com.corphish.nightlight.design.fragments.base.BaseBottomSheetDialogFragment
 import com.corphish.nightlight.helpers.ExternalLink
 import kotlinx.android.synthetic.main.bottom_sheet_msg.*

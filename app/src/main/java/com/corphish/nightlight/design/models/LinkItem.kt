3773fca82222bc43bbd900afede08f9ecc8cde27
corphish <d97.avinaba@gmail.com>
2020-10-10 22:48:45 +0530

    LinkItem: Remove as it is unused
    Signed-off-by: corphish <d97.avinaba@gmail.com>

--- app/src/main/java/com/corphish/nightlight/design/models/LinkItem.kt
+++
@@ -1,6 +0,0 @@
-package com.corphish.nightlight.design.models
-
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-
-data class LinkItem(@StringRes val captionRes: Int, @DrawableRes val imageRes: Int, val link: String)

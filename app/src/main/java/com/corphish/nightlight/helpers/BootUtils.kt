ab679668421d5e66f558aa26a11f03912beb4acb
corphish <d97.avinaba@gmail.com>
2020-11-20 20:43:23 +0530

    BootUtils: Cleanup
    Signed-off-by: corphish <d97.avinaba@gmail.com>

--- app/src/main/java/com/corphish/nightlight/helpers/BootUtils.kt
+++ app/src/main/java/com/corphish/nightlight/helpers/BootUtils.kt
@@ -23,19 +23,12 @@ object BootUtils {
         val masterSwitch = PreferenceHelper.getBoolean(context, Constants.PREF_MASTER_SWITCH)
         val autoSwitch = PreferenceHelper.getBoolean(context, Constants.PREF_AUTO_SWITCH)
 
-        val sStartTime = PreferenceHelper.getString(context, Constants.PREF_START_TIME, Constants.DEFAULT_START_TIME)
-        val sEndTime = PreferenceHelper.getString(context, Constants.PREF_END_TIME, Constants.DEFAULT_END_TIME)
-
         if (!masterSwitch) return
-        if (sStartTime == null || sEndTime == null) return
 
         if (!autoSwitch) {
             Core.applyNightModeAsync(true, context)
             return
         }
-
-        val state = TimeUtils.isInRange(sStartTime, sEndTime)
-        Core.applyNightModeAsync(state, context)
 
         // The automate signal receiver handles the automation, so just ping it.
         AlarmUtils.setAlarmRelative(context, 0)
